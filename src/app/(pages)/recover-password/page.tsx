5e2d2f8fb361cc0afb34ac74047d503442a68a75
aluca94@gmail.com <alutza94@gmail.com>
2023-12-06 15:51:37 +0100

    add logo

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -6,12 +6,18 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
+    <section className={classes.recoverPassword}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="logo" width={250} height={23} />
+        </Link>
+      </div>
+    </section>
   )
 }
 
