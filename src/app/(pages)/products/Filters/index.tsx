5d67b15d95a9b3de22085b20f81ffccdf58e96f1
aluca94@gmail.com <alutza94@gmail.com>
2023-12-11 13:52:19 +0100

    add RadioBox

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -6,11 +6,15 @@ import classes from './index.module.scss'
 import { useFilter } from '../../../_providers/Filter'
 import { Category } from '../../../../payload/payload-types'
 import { Checkbox } from '../../../_components/Checkbox'
+import { HR } from '../../../_components/HR'
+import { RadioButton } from '../../../_components/RadioButton'
 
 const Filters = ({ categories }: { categories: Category[] }) => {
   const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
 
   const handleCategories = (categoryId: string) => {}
+  const handleSort = () => {}
+
   return (
     <div className={classes.filters}>
       {' '}
@@ -18,7 +22,7 @@ const Filters = ({ categories }: { categories: Category[] }) => {
         <h6 className={classes.title}> Product Categories</h6>
         <div className={classes.categories}>
           {categories.map(category => {
-            const isSelected = false
+            const isSelected = categoryFilters.includes(category.id)
 
             return (
               <Checkbox
@@ -30,6 +34,24 @@ const Filters = ({ categories }: { categories: Category[] }) => {
               />
             )
           })}
+        </div>
+        <HR className={classes.hr} />
+        <h6 className={classes.title}>Sort By </h6>
+        <div className={classes.categories}>
+          <RadioButton
+            label="Latest"
+            value="-createdAt"
+            isSelected={sort === '-createAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
+          <RadioButton
+            label="Oldest"
+            value="createdAt"
+            isSelected={sort === 'createAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
         </div>
       </div>
     </div>
