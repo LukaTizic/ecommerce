ccb7878bda582af6822a586164fc11c59be6ab7a
aluca94@gmail.com <alutza94@gmail.com>
2023-12-26 12:24:02 +0100

    revert

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
-
+  swcMinify: true,
   images: {
     domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL]
       .filter(Boolean)
